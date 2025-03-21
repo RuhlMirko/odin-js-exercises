7b40b662941387687e191cbe6fc1b42229d9f666
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-21 18:11:44 -0300

    Added completed exercises: 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, times) {
+    let newString = ''
+    for (let step = 0; step < times; step++) {
+        newString += string
+    }
+    return newString
 };
 
 // Do not edit below this line
