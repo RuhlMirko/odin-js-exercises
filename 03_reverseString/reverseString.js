bf48681844313421604d8a9ee61fbe11d050859e
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-21 18:15:39 -0300

    Added 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    const strLength = string.length
+    let newString = ''
+    for (let i = strLength - 1; i >= 0; i--) {
+        newString += string[i]
+    }
+    return newString
 };
 
 // Do not edit below this line
