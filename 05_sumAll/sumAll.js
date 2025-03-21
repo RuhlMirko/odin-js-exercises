5532c90aff7e02210588f023ab220f70f652124e
Mirko Ruhl <ruhlmirkojoel@gmail.com>
2025-03-21 18:24:20 -0300

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function (first, second) {
+    let allNums = 0
+    for (let i = first; i <= second; i++) {
+        allNums += i
+    }
+    return allNums
 };
 
 // Do not edit below this line
